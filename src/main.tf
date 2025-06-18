75d9538b118ae0af2829edbdabdaf2eea4dd96cf
Roman L <roman-ronin@yandex.ru>
2025-06-19 01:32:00 +0300

    4.1

--- src/main.tf
+++ src/main.tf
@@ -1,66 +1,16 @@
-module "vpc_mysql" {
-  source       = "./modules/vpc"
-  env_name     = var.vms_mod_marketing[0].env_name
-  subnets = [
-    { zone = var.default_zone_a, cidr = var.default_cidr_a },
-    { zone = var.default_zone_b, cidr = var.default_cidr_а },
-  ]
+resource "random_string" "unique_id" {
+  length  = 8
+  upper   = false
+  lower   = true
+  numeric = true
+  special = false
 }
 
-module "mysql_cluster" {
-  source       = "./modules/mysql_cluster"
-  cluster_name = "my-mysql-cluster"
-  network_id   = module.vpc_mysql.network_id
-  ha           = true
-  subnet_ids   = module.vpc_marketing.subnet_ids,
-  user_password = "secure_password_123"
-}
+module "s3" {
+  source = "../../"
 
-# module "marketing-vm" {
-#   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-#   env_name       = var.vms_mod_marketing[0].env_name
-#   network_id     = module.vpc_marketing.network_id
-#   subnet_zones   = var.vms_mod_marketing[0].subnet_zones
-#   subnet_ids     = module.vpc_marketing.subnet_ids
-#   instance_name  = var.vms_mod_marketing[0].instance_name
-#   instance_count = var.vms_mod_marketing[0].instance_count
-#   image_family   = var.vm_web_image_family
-#   public_ip      = var.vms_mod_marketing[0].public_ip
-
-#   labels = { 
-#     project = var.vms_mod_marketing[0].env_name
-#      }
-#   metadata = {
-#     user-data = data.template_file.cloudinit.rendered
-#     serial-port-enable = local.serial-port-enable
-#   }
-# }
-
-# module "analitics-vm" {
-#   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-#   env_name       = var.vms_mod_analitics[0].env_name
-#   network_id     = module.vpc_analitics.network_id
-#   subnet_zones   = var.vms_mod_analitics[0].subnet_zones
-#   subnet_ids     = module.vpc_analitics.subnet_ids
-#   instance_name  = var.vms_mod_analitics[0].instance_name
-#   instance_count = var.vms_mod_analitics[0].instance_count
-#   image_family   = var.vm_web_image_family
-#   public_ip      = var.vms_mod_analitics[0].public_ip
-
-#   labels = { 
-#     project = var.vms_mod_analitics[0].env_name
-#      }
-
-#   metadata = {
-#     user-data = data.template_file.cloudinit.rendered
-#     serial-port-enable = local.serial-port-enable
-#   }
-# }
-
-data "template_file" "cloudinit" {
-  template = file("./cloud-init.yml")
-
-    vars = {
-    ssh_public_key     = file(var.vms_ssh_root_key)
+  bucket_name = "s3bucket-${random_string.unique_id.result}"
+  versioning = {
+    enabled = true
   }
 }
